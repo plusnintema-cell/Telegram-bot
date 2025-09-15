4510f688ef1fd3e34797d752bfd4288ab0c34e9a
plusnintema-cell <plusnin.tema@yandex.ru>
2025-09-15 23:57:55 +0300

    Update main.py

--- main.py
+++ main.py
@@ -6,6 +6,9 @@ bot = telebot.TeleBot(TOKEN)
 
 @bot.message_handler(commands=['start'])
 def start_message(message):
-    bot.reply_to(message, "Бот запущен и работает 💛")
+    bot.reply_to(message, "бот запущен и работает 👋")
+
+# Сбрасываем вебхук, чтобы не было конфликта
+bot.remove_webhook()
 
 bot.infinity_polling()
